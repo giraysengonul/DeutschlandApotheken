9b9cf637e7e21888dedc5e5833abc1d536e773a9
hakkicansengonul <hakkicansengonul@gmail.com>
2024-03-08 15:08:13 +0300

    Setup

---
+++ DeutschlandApotheken/DeutschlandApotheken/Controllers/Core/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  DeutschlandApotheken
+//
+//  Created by Hakkı Can Şengönül on 8.03.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
